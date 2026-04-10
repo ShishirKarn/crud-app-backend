fb2d94e9e281f2f0fe779044ed0d844bc5e6062a
Shishir Karn <karnshishir22@gmail.com>
2026-04-10 13:49:27 +0530

    step 6

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ async function sendToToken(token, title, body) {
 }
 
 // Daily moring reminder
-cron.schedule('*/1 13 * * *',()=>{
+cron.schedule('* * * * *',()=>{
   db.query('SELECT firstName, fcm_token FROM users WHERE fcm_token IS NOT NULL',
     async(err,users)=>{
       if(err) return;
